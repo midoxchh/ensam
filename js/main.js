a949366e6a947f23bd0dfa98bb737f8f247381af
mehdi_lghzal <mohamedmehdichahboune@gmail.com>
2024-04-08 18:52:42 +0000

    change color

--- js/main.js
+++ js/main.js
@@ -2,8 +2,21 @@
 $(document).ready(function(){
 
     $('.fa-bars').click(function(){
+
         $(this).toggleClass('fa-times');
-        $('.navbar').toggleClass('nav-toggle')
+        $('.navbar').toggleClass('nav-toggle');
+
     });
-    window.on("scrool")
+
+    $(window).on('scroll load' , function(){
+        $('.fa-bars').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        if($(window).scrollTop() > 30){
+            $('header').addClass('header-active');
+        }else{
+            $('header').removeClass('header-active');
+        }
+    })
+    
 });
