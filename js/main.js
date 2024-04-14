655ca059440a2b7b1352a76b50b66064dbcabc33
mehdi_lghzal <mohamedmehdichahboune@gmail.com>
2024-04-14 14:25:32 +0100

    start editing the partenaires section

--- js/main.js
+++ js/main.js
@@ -1,83 +1,82 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $('.fa-bars').click(function(){
+  $('.fa-bars').click(function () {
 
-        $(this).toggleClass('fa-times');
-        $('.navbar').toggleClass('nav-toggle');
+    $(this).toggleClass('fa-times');
+    $('.navbar').toggleClass('nav-toggle');
 
+  });
+
+  $(window).scroll(function () {
+    $('.fa-bars').removeClass('fa-times');
+    $('.navbar').removeClass('nav-toggle');
+
+    if ($(window).scrollTop() > 30) {
+      $('header').addClass('header-active');
+    } else {
+      $('header').removeClass('header-active');
+    }
+
+    // $('section').each(function(){
+
+    //     var top = $(window).scrollTop();
+    //     var id = $(this).attr('id');
+    //     var height = $(this).height();
+    //     var top = $(this).offset().top - 200;
+
+    //     if(top >= offset && top < height + offset){
+    //         console.log('hello world ')
+    //         $('.navbar ul li a').removeClass('active');
+    //         $('.navbar').find('[href="#home"]').addClass('active');
+    //     }
+
+    // });
+
+    $('section').each(function () {
+      var top = $(window).scrollTop();
+      var id = $(this).attr('id');
+      var height = $(this).height();
+      var sectionTop = $(this).offset().top - 200;
+
+      if (top >= sectionTop && top < sectionTop + height) {
+        console.log('hello world');
+        $('.navbar ul li a').removeClass('active');
+        $('.navbar').find('[href="#' + id + '"]').addClass('active');
+      }
     });
-
-    $(window).scroll(function() {
-        $('.fa-bars').removeClass('fa-times');
-        $('.navbar').removeClass('nav-toggle');
-
-        if($(window).scrollTop() > 30){
-            $('header').addClass('header-active');
-        }else{
-            $('header').removeClass('header-active');
-        }
-
-        // $('section').each(function(){
-            
-        //     var top = $(window).scrollTop();
-        //     var id = $(this).attr('id');
-        //     var height = $(this).height();
-        //     var top = $(this).offset().top - 200;
-    
-        //     if(top >= offset && top < height + offset){
-        //         console.log('hello world ')
-        //         $('.navbar ul li a').removeClass('active');
-        //         $('.navbar').find('[href="#home"]').addClass('active');
-        //     }
-    
-        // });
-        
-        $('section').each(function() {
-            var top = $(window).scrollTop();
-            var id = $(this).attr('id');
-            var height = $(this).height();
-            var sectionTop = $(this).offset().top - 200;
-    
-            if (top >= sectionTop && top < sectionTop + height) {
-                console.log('hello world');
-                $('.navbar ul li a').removeClass('active');
-                $('.navbar').find('[href="#' + id + '"]').addClass('active');
-            }
-        });
-    });
+  });
 
 });
 
 
 // Disable right-click context menu
-document.addEventListener('contextmenu', function(event) {
+document.addEventListener('contextmenu', function (event) {
+  event.preventDefault();
+});
+
+// Disable keyboard shortcuts (CTRL + Shift + I, CTRL + Shift + J, CTRL + Shift + C)
+document.onkeydown = function (event) {
+  if (event.ctrlKey &&
+    (event.shiftKey && (event.keyCode === 73 || event.keyCode === 74 || event.keyCode === 67))
+  ) {
+    return false;
+  }
+};
+
+// Prevent inspecting elements by drag selection
+document.addEventListener('mousedown', function (event) {
+  if (event.button === 2) {
+    // Right click
     event.preventDefault();
-  });
+    return false;
+  }
+});
 
-  // Disable keyboard shortcuts (CTRL + Shift + I, CTRL + Shift + J, CTRL + Shift + C)
-  document.onkeydown = function(event) {
-    if (
-      event.ctrlKey &&
-      (event.shiftKey && (event.keyCode === 73 || event.keyCode === 74 || event.keyCode === 67))
-    ) {
-      return false;
-    }
-  };
-
-  // Prevent inspecting elements by drag selection
-  document.addEventListener('mousedown', function(event) {
-    if (event.button === 2) {
-      // Right click
-      event.preventDefault();
-      return false;
-    }
-  });
-
-  // Prevent inspecting elements using F12 key
-  window.addEventListener('keydown', function(event) {
-    if (event.keyCode === 123) { // F12 key
-      event.preventDefault();
-      return false;
-    }
-  });
+// Prevent inspecting elements using F12 key
+window.addEventListener('keydown', function (event) {
+  if (event.keyCode === 123) { // F12 key
+    event.preventDefault();
+    return false;
+  }
+});
